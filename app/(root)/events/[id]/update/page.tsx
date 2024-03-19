9529d0a8ea6bd0bbe46fc91a10f22d5bcde9507a
aark07 <aaryank098@gmail.com>
2024-03-19 20:35:41 +0530

    implimenting update Event, so user can update existing events

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -1,11 +1,21 @@
 import EventForm from '@/components/shared/EventForm'
+import { getEventById } from '@/lib/actions/event.actions'
 import { auth } from '@clerk/nextjs'
 import React from 'react'
 
-const UpdateEvent = () => {
+type UpdateEventProps = {
+  params: {
+    id: string
+  }
+}
+
+const UpdateEvent = async ({ params: { id }}: UpdateEventProps) => {
+
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string; 
   // ? is added if the sessionClaim doesnt exist
+
+  const event = await getEventById(id)
 
   return (
     <>
@@ -14,7 +24,12 @@ const UpdateEvent = () => {
       </section>
 
       <div className='wrapper my-8'>
-        <EventForm userId={userId} type="Update" /> 
+        <EventForm 
+          type="Update" 
+          event={event} 
+          eventId={event._id} 
+          userId={userId}
+        /> 
       </div>
     </>
   )
